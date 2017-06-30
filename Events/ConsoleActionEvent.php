afd501d5c9426ee320a1eb4b426930c7d4adb01d
serg <kulyamin_sergey@mail.ru>
2017-06-30 19:41:17 +0300

    -- default cli options

--- Events/ConsoleActionEvent.php
+++ Events/ConsoleActionEvent.php
@@ -110,6 +110,10 @@ class ConsoleActionEvent extends ActionEvent
         }
         foreach ((array) ($info[$this->action]['tags']['option'] ?? []) as $opt) {
             list($type, $name, $desc) = explode(' ', $opt);
+            if (Str :: pos('=', $name) !== false) {
+                list($name, $default) = explode('=', $name);
+                $this->payload['options'][$name] = $default;
+            }
             if (Str :: pos('*', $name) === 0) {
                 if (!isset($this->options[substr($name, 1)])) {
                     throw new InvalidArgumentException('Option ' . substr($name, 1) . ' is required');
